993bbf01f1367bd059a66f748aed74e9e6bc9294
Lakshyameel <lkmeelposani@gmail.com>
2025-03-29 18:36:14 +0530

    Delete blackcore/source code directory

--- blackcore/source code/app/data/data/data/app/data/app/app/app/models.py
+++
@@ -1,5 +0,0 @@
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False, unique=True)
-    password = db.Column(db.String(120), nullable=False)
-    role = db.Column(db.String(20), default='user')  # 'user' or 'admin'
